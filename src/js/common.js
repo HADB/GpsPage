4f01c5641da6fce1dc5c5fd7b756bc07e148fcc8
HADB <haiandengbin@126.com>
2016-03-12 20:26:45 +0800

    地图上的小车

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,41 @@
 $(function () {
-    var map = new BMap.Map("map-container");
-    var point = new BMap.Point(121.47519, 31.228833);
-    map.centerAndZoom(point, 16);
-    map.enableScrollWheelZoom(true);
-    map.addControl(new BMap.NavigationControl({offset: new BMap.Size(15, 70)}));
-    map.addControl(new BMap.MapTypeControl({offset: new BMap.Size(15, 70)}));
-    map.addControl(new BMap.PanoramaControl({offset: new BMap.Size(15, 100)}));
+    //绘制地图
+    function renderMap() {
+        var map = new BMap.Map("map-container");
+        var point = new BMap.Point(121.47519, 31.228833);
+        map.centerAndZoom(point, 16);
+        map.enableScrollWheelZoom(true);
+        map.addControl(new BMap.NavigationControl({offset: new BMap.Size(15, 70)}));
+        map.addControl(new BMap.MapTypeControl({offset: new BMap.Size(15, 70)}));
+        map.addControl(new BMap.PanoramaControl({offset: new BMap.Size(15, 100)}));
+
+        var infoWindowOptions = {
+            width: 230,     // 信息窗口宽度
+            height: 158,     // 信息窗口高度
+            title: '<h5 class="title">TY01-883591<i class="fa fa-battery-half pull-right"></i><i class="fa fa-signal pull-right"></i></h5>'
+        };
+        var $infoWindowContent = $('<div class="info-content"></div>');
+        $infoWindowContent.append('<h5><span>状态：</span>静止（1小时24分）</h5>');
+        $infoWindowContent.append('<h5><span>定位类型：</span>基站定位</h5>');
+        $infoWindowContent.append('<h5><span>信号：</span>2016/03/01 14:50:27</h5>');
+        $infoWindowContent.append('<h5><span>定位：</span>2016/03/01 11:41:19</h5>');
+        $infoWindowContent.append('<h5><a>街景</a><a>跟踪</a><a>回放</a><a>放大</a></h5>');
+
+        var infoWindow = new BMap.InfoWindow($infoWindowContent.prop('outerHTML'), infoWindowOptions);  // 创建信息窗口对象
+
+        var carIcon = new BMap.Icon("img/car.png", new BMap.Size(50, 26), {
+            offset: new BMap.Size(25, 13),
+            imageOffset: new BMap.Size(0, 0)
+        });
+        var marker = new BMap.Marker(point, {icon: carIcon});
+
+        marker.addEventListener("click", function () {
+            map.openInfoWindow(infoWindow, point);      // 打开信息窗口
+        });
+        map.addOverlay(marker);
+    }
+
+    renderMap();
 
     //自定义滚动条样式
     $('.custom-scrollbar').perfectScrollbar({suppressScrollX: true});
