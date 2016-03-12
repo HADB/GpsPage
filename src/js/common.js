40d110cd6414fb384a506200030c47ddef730be4
HADB <haiandengbin@126.com>
2016-03-12 16:59:08 +0800

    左侧组动画

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('.custom-scrollbar').perfectScrollbar();
+    $('.custom-scrollbar').perfectScrollbar({suppressScrollX: true});
     var map = new BMap.Map("map-container");
     var point = new BMap.Point(121.47519, 31.228833);
     map.centerAndZoom(point, 16);
@@ -15,5 +15,31 @@ $(function () {
         else {
             $(this).addClass('checked');
         }
-    })
+    });
+
+    $('.group-tag').click(function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.user').hover(function () {
+        $('.user').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.group-name').click(function () {
+        var $group = $(this).parent();
+        if ($group.hasClass('expanded')) {
+            $group.find('.user').removeClass('active');
+            $group.find('.users').slideUp();
+            $group.removeClass('expanded');
+        }
+        else {
+            $group.siblings('.group').find('.user').removeClass('active');
+            $group.siblings('.group').find('.users').slideUp();
+            $group.siblings('.group').removeClass('expanded');
+            $group.addClass('expanded');
+            $group.find('.users').slideDown();
+        }
+    });
 });
